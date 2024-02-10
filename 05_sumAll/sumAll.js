7753d01c41103cef93cb34fcb3f3285d9248bcbe
Luan Balbi <49569950+luanbalbi@users.noreply.github.com>
2024-02-09 22:25:02 -0300

    Exercise 05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+function isNumber(value) {
+    return typeof value === 'number';
+  }
 
+const sumAll = function(number1, number2) {
+    if (number1 < 0 || number2 < 0) return "ERROR";
+    if (!isNumber(number1) || !isNumber(number2)) return "ERROR";
+    let sum = 0;
+    if (number1 > number2) {
+        const temp = number1;
+        number1 = number2;
+        number2 = temp;
+    }
+    for (let index = number1; index <= number2; index++) {
+        sum += index;
+    }
+    return sum;
 };
 
 // Do not edit below this line
