ea0185da9b2821b6a7f11135dcd78a4bfb3ffe3a
Luan Balbi <49569950+luanbalbi@users.noreply.github.com>
2024-02-09 22:10:36 -0300

    Exercise 04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    console.log(args)
+    for (let i = 0; i < args.length; i++) {
+        while (array.indexOf(args[i]) != -1) {
+            array.splice(array.indexOf(args[i]), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
