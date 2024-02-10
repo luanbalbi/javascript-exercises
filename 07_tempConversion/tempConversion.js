28889871a74159b27bee5a95aedec9245e70160c
Luan Balbi <49569950+luanbalbi@users.noreply.github.com>
2024-02-09 22:45:02 -0300

    Exercise 07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  const celsius = Math.round((temperature - 32) * 5/9*10)/10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  const fahrenheit = Math.round((temperature * (9/5) + 32)*10)/10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
@@ -9,3 +13,8 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+/*
+	from Celsius to Fahrenheit	x °C ≘ (x × 9/5 + 32) °F
+  to Celsius from x °F ≘ (x − 32) × 5/9 °C
+*/
